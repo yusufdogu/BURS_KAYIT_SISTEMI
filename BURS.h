bba6703df529ee87fc13e8175806bc24fbc45147
Yusuf DOĞU <148912729+yusufdogu@users.noreply.github.com>
2024-03-15 13:49:36 +0300

    KAYIT ->FONKSİYON PROTOTİPLERİ

---
+++ BURS.h
@@ -0,0 +1,13 @@
+//
+// Created by ydogu on 7.03.2024.
+//
+
+#ifndef UNTITLED_BURS_H
+#define UNTITLED_BURS_H
+int menu();
+void kayit();
+void odeme();
+void odenmeyen();
+void odenen();
+void sifirla();
+#endif //UNTITLED_BURS_H
